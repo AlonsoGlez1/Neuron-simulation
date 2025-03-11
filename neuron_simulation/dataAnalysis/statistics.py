e1dfac968ec0a453dd75a1323b81495d6f828324
AlonsoGlez1 <a.gonzalezescobedo@ugto.mx>
2025-03-11 15:45:34 -0600

    Improved Statistical Analysis

--- neuron_simulation/dataAnalysis/statistics.py
+++ neuron_simulation/dataAnalysis/statistics.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 
 # Path to directory containing the files
-file_directory = os.path.join("simulations", "5k_lattice_150Lx_0,5rad_0,17453pac_10e5time_1branch_2synapses") # Relative path
+file_directory = os.path.join("..", "simulations", "5k_random_100Lx_0,5rad_0,30270pac_10e5time_1branch_2synapses") # Relative path
 
 # File pattern for all the data files
 file_pattern = os.path.join(file_directory, "connections_dat_*")
@@ -29,7 +29,7 @@ for file in files:
         column_data[col].append(df[col].values)
 
 # Perform statistical analysis and write results for each column
-output_directory = "simulations/lattice_150_statistics"
+output_directory = "simulations/random_100_statistics"
 os.makedirs(output_directory, exist_ok=True)
 
 for col, data in column_data.items():
@@ -40,11 +40,16 @@ for col, data in column_data.items():
     mean = np.mean(combined_data, axis=1)
     std_dev = np.std(combined_data, axis=1)
     
+    # Calculate Standard Error of the Mean (SEM)
+    N = combined_data.shape[1]  # Number of simulations (files)
+    sem = std_dev / np.sqrt(N)
+    
     # Create a DataFrame for output
     results_df = pd.DataFrame({
         "timeSteps": time_steps,
         "mean": mean,
-        "std_dev": std_dev
+        "std_dev": std_dev,
+        "sem": sem
     })
     
     # Save to a file named after the column
